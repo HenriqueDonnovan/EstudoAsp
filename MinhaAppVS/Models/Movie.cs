f9519f5e07a3d0d18f92b08674d0920ae2fe6d57
Zylix - Donnovan <donnovan@zylix.local>
2022-10-19 17:27:18 -0300

    Iniciando o versionamento de arquivos

---
+++ MinhaAppVS/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinhaAppVS.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+      
+        [Required(ErrorMessage = "This field must be filled")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "The title must have between 3 and 60 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage ="The field date must be filled")]
+        [DataType(DataType.DateTime, ErrorMessage = "Incorrect format date")]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-|u00FF""'\w-]*$", ErrorMessage = "Invalid format")]
+        [StringLength(60, ErrorMessage = "Max is 60 characters")]
+        public string Genre { get; set; }
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Between 1 and 1000")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Value { get; set; }
+
+        [DisplayName("Avaliation")]
+        public string Avaliation { get; set; }
+    }
+}
